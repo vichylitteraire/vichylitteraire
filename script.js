cc6eef99238b1a16c192269e6372ad91ade7dcd3
vichylitteraire <vichylitteraire@gmail.com>
2026-02-05 08:33:18 -0800

    script.js

--- script.js
+++ script.js
@@ -91,8 +91,8 @@ function applyAds() {
         },
         'colada': {
             text: { 
-                fr: "❀ Vous écrivez ? Votre texte pourrait être lu ici-même, par les clients de nos cafés partenaires.\n\nPartagez votre talent avec nous ! Envoyez vos textes par email ❀", 
-                en: "❀ Do you write? Your text could be read right here by the guests of our partner cafes.\n\nShare your talent with us! Send your stories via email ❀" 
+                fr: "❀❀❀ \n\n Vous écrivez ? Votre texte pourrait être lu ici-même, par les clients de nos cafés partenaires.\n\nPartagez votre talent avec nous ! Envoyez vos textes par email", 
+                en: "❀❀❀ \n\n Do you write? Your text could be read right here by the guests of our partner cafes.\n\nShare your talent with us! Send your stories via email" 
             },
             url: "#"
         }
