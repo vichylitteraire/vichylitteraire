4343aad2e2f2ff730291a43c527cea3de748096c
vichylitteraire <vichylitteraire@gmail.com>
2026-07-18 10:19:55 -0700

    stories_fr.js

--- stories_fr.js
+++ stories_fr.js
@@ -3,8 +3,8 @@ const STORIES_DATA = {
         labelAuthor: "Par",
         nextBtn: "Autre histoire 🎲",
         contactLabel: "Pour nous contacter :",
-        adText: "Libérer ses écrits proposes des ateliers d'écriture et un accompagnement à la biographie à Vichy et en ligne ",
-        adLink: "En savoir plus",
+        adText: "VICHY LITTÉRAIRE",
+        /* adLink: "En savoir plus", */
         stories: [
             { 
                 title: "CONFESSION", 
